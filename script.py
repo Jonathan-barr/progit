ad37df772dab6d41429e8728bf2db2ba328d3c59
Jonathan-barr <jonathan_barrionuevo@hotmail.com>
2020-12-21 18:24:01 -0500

    Commit Inicial

---
+++ script.py
@@ -0,0 +1,8 @@
+# Script de prueba para Git
+# Autor: Jonathan Barrionuevo <jbarrionuevop@est.ups.edu.ec>
+# Fecha: 21/12/2020
+# Versión: 1.0.0
+def main():
+print("Hola Mundo!")
+if __name__ == "__main__":
+main()
